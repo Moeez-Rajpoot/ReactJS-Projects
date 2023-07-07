9f61f2997714c169b8e43102efd7f31bb88a6bfb
Moeez Rajpoot <99912138+Moeez-Rajpoot@users.noreply.github.com>
2023-07-06 23:26:16 -0700

    Updated

--- my-app/src/Components/Navbar.js
+++ my-app/src/Components/Navbar.js
@@ -32,7 +32,7 @@ export default function Navbar(props) {
       </ul>
       <form className="d-flex" role="search">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
+        <button className="btn btn-primary" type="submit">Search</button>
       </form>
     </div>
   </div>
--- my-app/src/Components/Textbox.js
+++ my-app/src/Components/Textbox.js
@@ -2,26 +2,37 @@ import React , {useState} from 'react'
 
 export default function Textbox(props) {
 
-    const [text, settxt] = useState('Enter Your Text here :');
+    const [text, settxt] = useState('');
 
+    window.onload=()=>{
+     document.getElementById('txtbox').style.borderColor= "black";
+
+    }
     const txtchange = function textchange(params) {
-        console.log("hello");
         settxt(params.target.value);
     }
 
     const clicked = ()=>{
-
         let val = text.toUpperCase();
+        settxt(val);
+    }
+    const lower = ()=>{
+        let val = text.toLowerCase();
         settxt(val);
     }
 
 
   return (
-  <div className="container mb-3">
+  <div className="container mb-2">
             <h1>{props.heading}</h1>
-            <textarea className="form-control" value={text} onChange={txtchange} id="txtbox"  rows="8"></textarea>
+            <textarea className="form-control " value={text} onChange={txtchange} id="txtbox"  rows="8"></textarea>
             <br/>
-            <button onClick={clicked} className="btn btn-primary">Uppercase</button>     
+            <button onClick={clicked} className="btn btn-primary mx-1">Uppercase</button>  
+            <button onClick={lower} className="btn btn-primary mx-2">Lowercase</button> 
+            <button onClick={clicked} className="btn btn-primary mx-2">Reset</button> 
+            <button onClick={clicked} className="btn btn-primary mx-2">Uppercase</button> 
+            <button onClick={clicked} className="btn btn-primary mx-2">Uppercase</button> 
+            <button onClick={clicked} className="btn btn-primary mx-2">Uppercase</button>    
   </div>
   
   ) 
